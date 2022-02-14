19296995802ea899341f88973e5c7c3ab43bd66d
rzkyadhi <rizky.nugroho@students.amikom.ac.id>
2022-02-14 17:45:18 +0700

    Create database and Migrations

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+class Room(models.Model):
+    name = models.CharField(max_length=1000)
+
+class Message(models.Model):
+    content = models.TextField()
+    date = models.DateTimeField(default=datetime.now, blank=True)
+    user = models.CharField(max_length=1000)
+    room = models.CharField(max_length=1000)
