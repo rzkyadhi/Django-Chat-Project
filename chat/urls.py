2213caf439994f6f3013c196692ffdd16d44930b
rzkyadhi <rizky.nugroho@students.amikom.ac.id>
2022-02-15 09:10:54 +0700

    Adding Home Function

--- chat/urls.py
+++ chat/urls.py
@@ -3,5 +3,6 @@ from .import views
 
 urlpatterns = [
     path('', views.home, name = 'home'),
-    path('room/', views.room, name = 'room'),
+    path('<str:room>/', views.room, name = 'room'),
+    path('checkview', views.checkview, name = 'checkview')
 ]
--- chat/views.py
+++ chat/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from chat.models import Room, Message
 
 def home(request):
     return render(request, 'home.html')
 
-def room(request):
+def room(request, room):
     return render(request, 'room.html')
+
+def checkview(request):
+    room = request.POST['RoomCode']
+    username = request.POST['Username']
+
+    if Room.objects.filter(name = room).exists():
+        return redirect('/'+ room + '/?username=' + username)
+    else:
+        new_room = Room.objects.create(name = room)
+        new_room.save()
+        return redirect('/'+ room + '/?username=' + username)
