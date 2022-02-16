2afe768c70d9ef721b6d2342b1f70dbacd39cf13
rzkyadhi <rizky.nugroho@students.amikom.ac.id>
2022-02-16 08:54:56 +0700

    Sending Message to DB

--- chat/urls.py
+++ chat/urls.py
@@ -1,8 +1,10 @@
+from unicodedata import name
 from django.urls import path
 from .import views
 
 urlpatterns = [
     path('', views.home, name = 'home'),
     path('<str:room>/', views.room, name = 'room'),
-    path('checkview', views.checkview, name = 'checkview')
+    path('checkview', views.checkview, name = 'checkview'),
+    path('send', views.send, name = 'send')
 ]
--- chat/views.py
+++ chat/views.py
@@ -1,11 +1,19 @@
+from multiprocessing.sharedctypes import Value
 from django.shortcuts import render, redirect
 from chat.models import Room, Message
+from django.http import HttpResponse
 
 def home(request):
     return render(request, 'home.html')
 
 def room(request, room):
-    return render(request, 'room.html')
+    username = request.GET.get('username')
+    room_details = Room.objects.get(name = room)
+    return render(request, 'room.html', {
+        'username': username,
+        'room': room,
+        'room_details': room_details
+    })
 
 def checkview(request):
     room = request.POST['RoomCode']
@@ -17,3 +25,12 @@ def checkview(request):
         new_room = Room.objects.create(name = room)
         new_room.save()
         return redirect('/'+ room + '/?username=' + username)
+
+def send(request):
+    content = request.POST['content']
+    room_id = request.POST['room_id']
+    username = request.POST['username']
+
+    new_message = Message.objects.create(content = content, user = username, room = room_id)
+    new_message.save()
+    return HttpResponse('Message Sent !')
